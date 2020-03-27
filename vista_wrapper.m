c1dcd7e155b61a1d3bdbadb2a8e864b8c3fd8a0a
Jan Kurzawski <jankurzawski@MacBook-Pro.local>
2020-03-27 10:05:07 -0400

    remove unecessary files

--- vista_wrapper.m
+++ vista_wrapper.m
@@ -10,25 +10,26 @@ addpath(genpath('./package/')) % consider adding this to external of mritools
 
 session             = 'nyu3t01';
 subject             = 'wlsubj042';
-runnum              = 99;
+runnumber           = 99;
 task                = 'prf';
 
 
 projectDir          = sprintf('./../BIDS/'); % path to folder with derivatives
 apertureFolder      = sprintf('%sStimuli',projectDir);
+% apertureFolder      = sprintf('%sderivatives/stim_apertures',projectDir);
+
 filesDir            = sprintf('%sderivatives/fmriprep/sub-%s/ses-%s/func',projectDir,subject,session);
+filesDir_wrong      = sprintf('%ssub-%s/ses-%s/func/',projectDir,subject,session);
+%% path2configs 
 
-%% path2configs
+cfg.param               = sprintf('%ssub-%s_ses-%s_task-%s_acq-normal_run-%i_cfg.json',filesDir_wrong,subject,session,task,runnumber);
+cfg.parambold           = sprintf('%ssub-%s_ses-%s_task-%s_acq-normal_run-%i_bold.json',filesDir_wrong,subject,session,task,runnumber);
+cfg.events              = sprintf('%ssub-%s_ses-%s_task-%s_events.json',filesDir_wrong,subject,session,task);
+cfg.events_tsv          = sprintf('%ssub-%s_ses-%s_task-%s_events.tsv',filesDir_wrong,subject,session,task);
+cfg.average_filename    = sprintf('%ssub-%s/ses-%s/func/sub-%s_ses-%s_task-%s_acq-normal_run-%i',projectDir,subject,session,subject,session,task,runnumber);
+cfg.load                = 0; % create default cfg file (NYU settings)
 
-cfg.param               = sprintf('sub-%s_ses-%s_task-%s_acq-normal_run-%i_cfg.json',subject,session,task,runnum);
-cfg.parambold           = sprintf('sub-%s_ses-%s_task-%s_acq-normal_run-%i_bold.json',subject,session,task,runnum);
-cfg.events              = sprintf('sub-%s_ses-%s_task-%s_events.json',subject,session,task);
-cfg.events_tsv          = sprintf('sub-%s_ses-%s_task-%s_events.tsv',subject,session,task);
-
-%% add an aperture file
-% copyfile([apertureFolder filesep 'apertures.nii.gz'],...
-%     [apertureFolder filesep sprintf('sub-%s_ses-%s_task-%s_apertures.nii.gz',...
-%     param.subjectName,param.sessionName,parambold.TaskName)])
+debug               = 1; % fit only 100 voxels to see if code runs
 
 %% convert to mgz using freesurfer
 
@@ -44,13 +45,15 @@ for ii = 1:length(d)
     
 end
 
-runnums           =  ones(1,length(d)/2);
+runnums           =  1:length(d)/2; % / because there are 2 hemi
 dataFolder        = 'fmriprep';
 dataStr           = 'fsnative*.mgz';
 
 
+
+
 % results = bidsVistaPRF(projectDir,subject,session,task,runnums,dataFolder,dataStr,apertureFolder,filesDir,cfg);
-results = bidsVistaPRF(projectDir,subject,session,task,runnums,dataFolder,dataStr,apertureFolder,filesDir);
+results = bidsVistaPRF(projectDir,subject,session,task,runnums,dataFolder,dataStr,apertureFolder,filesDir,cfg,debug);
 
 
 
